80336cce5e53f821da964a245634d5ba8973c52c
Thanh61TH5 <giangthanh25092001@gmail.com>
2023-04-15 22:05:03 +0700

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	index.html

--- js/script.js
+++ js/script.js
@@ -32,5 +32,24 @@ showSlides(slideIndex = 0);
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+var form = document.querySelector('form');
+var submitBtn = document.querySelector('input[type="submit"]');
+var notify = document.getElementById('notify_box');
 
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
+    if (form.checkValidity()) {
+        setTimeout(() =>{
+            notify.style.display = 'inline-block';},700);
+        // Thêm lớp fade-out để mờ dần
+        notify.classList.add('fade-out');
+        // Sau 2 giây, remove lớp fade-out và đặt lại style.display
+        setTimeout(() => {
+            notify.classList.remove('fade-out');
+            notify.style.display = 'none';
+        }, 3000);
+    } else {
+        form.reportValidity();
+    }
+});
